e8c3420da998becba8063b915b282f9c992ead47
saikrishna <saikrishna.a.dev@gmail.com>
2024-02-08 19:35:27 +0530

    leetcode75/ (1768. Merge Strings Alternately)

---
+++ src/main/java/StringMerger.java
@@ -0,0 +1,18 @@
+public class StringMerger {
+    public static String mergeStringsAlternatively(String firstString, String secondString){
+        StringBuilder mergedString = new StringBuilder();
+        int firstStringLength = firstString.length();
+        int secondStringLength = secondString.length();
+        int minLength = Math.min(firstStringLength,secondStringLength);
+
+        for(int index = 0; index < minLength; index++){
+            mergedString.append(firstString.charAt(index));
+            mergedString.append(secondString.charAt(index));
+        }
+
+        mergedString.append(firstString,minLength,firstStringLength);
+        mergedString.append(secondString,minLength,secondStringLength);
+
+        return mergedString.toString();
+    }
+}
