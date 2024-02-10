6c8d4d30d49fb50ed6485688f44e142a648b5104
Saikrishna A <saikrishna.a.dev@gmail.com>
2024-02-10 11:44:56 +0530

    Add solution for LeetCode 238. Product of Array Except Self

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayUtils {
@@ -30,7 +31,7 @@ public class ArrayUtils {
                     plantedFlowersCount++;
                 }
             }
-            if(plantedFlowersCount >= newFlowersCount){
+            if (plantedFlowersCount >= newFlowersCount) {
                 return true;
             }
         }
@@ -52,5 +53,34 @@ public class ArrayUtils {
             value = flowerbed[index + 1];
         }
         return value;
+    }
+
+    public static int[] productExceptSelf(int[] numbers) {
+        int size = numbers.length;
+        int[] preProduct = new int[size];
+        int[] postProduct = new int[size];
+        int[] productExceptSelf = new int[size];
+        int product = 1;
+
+        preProduct[0] = 1;
+        postProduct[size - 1] = 1;
+        productExceptSelf[0] = 1;
+
+        for (int index = 1; index < size; index++) {
+            product = product * numbers[index - 1];
+            preProduct[index] = product;
+        }
+
+        product = 1;
+        for (int index = size - 2; index >= 0; index--) {
+            product = product * numbers[index + 1];
+            postProduct[index] = product;
+        }
+
+        for (int index = 0; index < size; index++) {
+            productExceptSelf[index] = preProduct[index] * postProduct[index];
+        }
+
+        return productExceptSelf;
     }
 }
