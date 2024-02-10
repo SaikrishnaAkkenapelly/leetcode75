100a9333514903f3a9926dc290d675295454dd2b
Saikrishna A <saikrishna.a.dev@gmail.com>
2024-02-10 09:17:21 +0530

    Solution for LeetCode 605. Can Place Flowers

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -17,4 +17,37 @@ public class ArrayUtils {
 
         return kidsWithGreatestCandies;
     }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int newFlowers) {
+        int totalFlowersThatCanBePut = 0;
+
+        for (int index = 0; index < flowerbed.length; index++) {
+            if (flowerbed[index] == 0) {
+                int previousIndexValue = getPreviousIndexValue(index, flowerbed);
+                int nextIndexValue = getNextIndexValue(index, flowerbed);
+                if (previousIndexValue == 0 && nextIndexValue == 0) {
+                    flowerbed[index] = 1;
+                    totalFlowersThatCanBePut++;
+                }
+            }
+        }
+
+        return newFlowers <= totalFlowersThatCanBePut;
+    }
+
+    private static int getPreviousIndexValue(int currentIndex, int[] flowerbed) {
+        int value = 0;
+        if (currentIndex - 1 >= 0) {
+            value = flowerbed[currentIndex - 1];
+        }
+        return value;
+    }
+
+    private static int getNextIndexValue(int currentIndex, int[] flowerbed) {
+        int value = 0;
+        if (currentIndex + 1 < flowerbed.length) {
+            value = flowerbed[currentIndex + 1];
+        }
+        return value;
+    }
 }
